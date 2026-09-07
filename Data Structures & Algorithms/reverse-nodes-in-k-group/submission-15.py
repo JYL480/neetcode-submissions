2abfdf041f1bb4587739e5351d7555d68ee12e2a
Lee Jun Yang <106604224+JYL480@users.noreply.github.com>
2026-09-07 13:02:04 +0800

    Add: reverse-nodes-in-k-group - submission-15

---
+++ Data Structures & Algorithms/reverse-nodes-in-k-group/submission-15.py
@@ -0,0 +1,86 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+"""
+0. lets redo this stupid question yah
+- What do they wan ?
+- They want to keep revesering every kth group and join back
+- Fkin stupid
+- Well the intuition is that we hav the??? the wafd?
+- We can use the reveser node thingy
+- And the get_kth function that reduces the value of k iteratively
+- Also note that we need to use some ummy node for this yah
+- where I will sever getkth revese and reattach and move up somehting liek that 
+
+
+
+"""
+
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        # Because the OG head changes, wso we will have to create a dummy node for this yh 
+        dummy = ListNode(0, head)
+        # Note how this pointns yah it is a new node right before
+        prev_grp = dummy # So in thee code later you will have to be prev_grp.next something liek that
+
+        while True: # Yah from what I rmb it is while true with a break yah inside, break when there is very liittle number of nodes left to reveser
+            
+            # So i got 3 pieces. I got to combine them tgt???
+
+            kth = self.get_kth(prev_grp.next, k)
+
+            if kth == None:
+                break
+            
+            # have to sever them 
+            next_grp = kth.next
+            kth.next = None
+
+            # But I want to keep prev_grp 
+            old_head = prev_grp.next
+
+            new_head_reversed = self.reverse(old_head)
+
+            prev_grp.next = new_head_reversed
+
+            old_head.next = next_grp
+
+            prev_grp = old_head
+
+        return dummy.next
+
+
+
+        
+
+    def reverse(self, head):
+        prev, curr = None, head
+
+        while curr:
+            tmp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = tmp
+
+        return prev
+
+    def get_kth(self, head, k):
+        # How do this shit, we get kth?? IDK 
+        # URMMMMM what do we do?
+        curr = head
+        # This question want use to keep movingtand ffind the pointer at which we are at the correct node??
+
+        while curr and k>1:
+            curr = curr.next
+            k -= 1
+        
+        return curr
+
+
+
+
+
